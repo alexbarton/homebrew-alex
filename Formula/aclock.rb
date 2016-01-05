1de81db8320940f168c5797c8401062dc55801e2
Alexander Barton <alex@barton.de>
2016-01-06 00:11:18 +0100

    aclock: Fix download URL
    Signed-off-by: Alexander Barton <alex@barton.de>

--- Formula/aclock.rb
+++ Formula/aclock.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Aclock < Formula
-  url 'http://www.tenox.net/out/aclock-unix-curses.c'
+  url 'http://www.tenox.net/out/aclock/aclock-unix-curses.c'
   sha1 '87b15f6030e27de3ac689e01120db2dd21003b3c'
   version '2.3'
   homepage 'http://www.tenox.net/out/'
