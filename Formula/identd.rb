ad3c2a109177141e86826afa2f620fa6b67d7cab
Alexander Barton <alex@barton.de>
2016-01-15 10:45:34 +0100

    identd: Fix building without OS X 10.7 SDK
    Remote "SDKROOT" variables from the Xcode project file, so Xcode
    automatically picks the current SDK when building the project.
    
    Closes #1.
    
    Signed-off-by: Alexander Barton <alex@barton.de>

--- Formula/identd.rb
+++ Formula/identd.rb
@@ -42,3 +42,39 @@ index fdd43b0..954e9ee 100644
  If you create a /usr/local/etc/identd.user file containing a username on a
  single line, that username will be returned if no user would be found otherwise.
  This is useful if you are behind a NAT router that causes the source and
+diff --git a/identd.xcodeproj/project.pbxproj b/identd.xcodeproj/project.pbxproj
+index 8339234..1fef724 100644
+--- a/identd.xcodeproj/project.pbxproj
++++ b/identd.xcodeproj/project.pbxproj
+@@ -190,7 +190,6 @@
+				MACOSX_DEPLOYMENT_TARGET = 10.6;
+				ONLY_ACTIVE_ARCH = YES;
+				PROVISIONING_PROFILE = "";
+-				SDKROOT = macosx10.7;
+			};
+			name = Debug;
+		};
+@@ -207,7 +206,6 @@
+				MACOSX_DEPLOYMENT_TARGET = 10.6;
+				ONLY_ACTIVE_ARCH = NO;
+				PROVISIONING_PROFILE = "";
+-				SDKROOT = macosx10.7;
+			};
+			name = Release;
+		};
+@@ -225,7 +223,6 @@
+				INSTALL_PATH = /usr/local/sbin;
+				PRODUCT_NAME = "$(TARGET_NAME)";
+				PROVISIONING_PROFILE = "";
+-				SDKROOT = macosx10.7;
+			};
+			name = Debug;
+		};
+@@ -246,7 +243,6 @@
+				ONLY_ACTIVE_ARCH = NO;
+				PRODUCT_NAME = "$(TARGET_NAME)";
+				PROVISIONING_PROFILE = "";
+-				SDKROOT = macosx10.7;
+			};
+			name = Release;
+		};
