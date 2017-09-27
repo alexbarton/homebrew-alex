5a347bc4c9cdb7fc27ccf218f1b9afc0ac8ab342
Alexander Barton <alex@barton.de>
2017-09-27 22:32:56 +0200

    aclock: Update download URL

--- Formula/aclock.rb
+++ Formula/aclock.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Aclock < Formula
-  url 'http://www.tenox.net/out/aclock/aclock-unix-curses.c'
+  url 'https://raw.githubusercontent.com/tenox7/aclock/master/sources/aclock-unix-curses.c'
   sha256 '2b098996409c4740f492fb8fd5a63cb5e3e15283c2f7e3f06c75d6a9ad916669'
   version '2.3'
   homepage 'http://www.tenox.net/out/'
