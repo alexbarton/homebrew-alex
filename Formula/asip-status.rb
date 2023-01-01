4b829903a13754cf3682e4e46e90d189fb655cb3
Alexander Barton <alex@barton.de>
2023-01-01 19:43:28 +0100

    asip-status v20211121
    Update to version of 2021-11-21, include this version in the "--version"
    output and actually test the script.

--- Formula/asip-status.rb
+++ Formula/asip-status.rb
@@ -3,19 +3,19 @@ require 'formula'
 class AsipStatus < Formula
   url 'https://raw.github.com/Netatalk/Netatalk/master/contrib/shell_utils/asip-status.pl.in'
   homepage 'http://netatalk.sourceforge.net/'
-  sha256 'c4b5129f0a99a25937065d1668068c1c736d08535e4fc60ff9e748b79249b2e9'
-  version '20120724'
+  sha256 '2acb0d89098b245ac130d16daca76a79fdbf6557ab58c88f6c379ceb059075a0'
+  version '20211121'
 
   def install
     system 'mv "asip-status.pl.in" "asip-status.pl"'
     inreplace "asip-status.pl",
       "@PERL@", "/usr/bin/perl"
     inreplace "asip-status.pl",
-      " @NETATALK_VERSION@", ""
+      "@NETATALK_VERSION@", version
     bin.install('asip-status.pl')
   end
 
   def test
-    system "bash", "-c", "#{bin}/asip-status.pl --version; test $? -eq 255"
+    system "bash", "-c", "#{bin}/asip-status.pl --version | grep -F Netatalk >/dev/null"
   end
 end
