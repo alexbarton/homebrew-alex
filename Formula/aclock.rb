66c91a39524b40f759e8ea30d5220e959cf8656d
Alexander Barton <alex@barton.de>
2025-10-24 12:26:42 +0200

    aclock: Update URL of the homepage

--- Formula/aclock.rb
+++ Formula/aclock.rb
@@ -4,7 +4,7 @@ class Aclock < Formula
   url 'https://raw.githubusercontent.com/tenox7/aclock/master/sources/aclock-unix-curses.c'
   sha256 '2b098996409c4740f492fb8fd5a63cb5e3e15283c2f7e3f06c75d6a9ad916669'
   version '2.3'
-  homepage 'http://www.tenox.net/out/'
+  homepage 'https://github.com/tenox7/aclock'
 
   def install
     system "cc aclock-unix-curses.c -o aclock -lcurses -lm"
