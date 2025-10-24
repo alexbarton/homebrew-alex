798f7b6d5ceee2613eb73bb6716e272903f0fe32
Alexander Barton <alex@barton.de>
2025-10-24 12:24:54 +0200

    asip-status: Update URL of the homepage

--- Formula/asip-status.rb
+++ Formula/asip-status.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class AsipStatus < Formula
   url 'https://raw.githubusercontent.com/Netatalk/netatalk/refs/tags/netatalk-4-3-2/contrib/bin_utils/asip-status.pl'
-  homepage 'http://netatalk.sourceforge.net/'
+  homepage 'https://netatalk.io/manual/en/asip-status.1'
   sha256 'c4ab5f6d94f93f672764247b4ce849b1c75531c82120f7621e66269d1d85d490'
   version '4.3.2'
 
